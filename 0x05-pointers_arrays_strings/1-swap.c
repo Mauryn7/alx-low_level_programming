60519ee511f1d231fc81ea305e31b998b9f839ff
Mauryn7 <moryn.mbugua@gmail.com>
2022-04-21 12:17:20 -0700

    My first commit for task 1 0x05-pointers_arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap numbers function
+ * @a: integer
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
