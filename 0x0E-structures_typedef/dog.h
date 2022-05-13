6c48816b831dabb751d7a6f620fdf3d4248721a5
Mauryn7 <moryn.mbugua@gmail.com>
2022-05-13 12:09:58 -0700

    My commit for task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,6 +11,7 @@ typedef struct dog dog_t;
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+
  *
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
@@ -21,3 +22,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
